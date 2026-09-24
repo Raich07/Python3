f38ef5722d79444211add3c6d250b1d2c0fa7b44
Raich07 <lopezsaezramon70@gmail.com>
2026-09-24 14:27:26 +0200

    practica7.py

---
+++ Tipos de datos simples/practica7.py
@@ -0,0 +1,5 @@
+masa = float(input("¿Cuanto pesas? (ponlo en kg):"))
+estatura = float(input("¿Cuanto mides? (ponlo en metros):"))
+total = masa/(estatura**2)
+print(f"El resultado es {round(total,2)}")
+
